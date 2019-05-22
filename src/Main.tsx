0a02479512d63bf69e743f46590d7b6cf6a2afbc
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2019-05-21 21:42:05 -0400

    added state properties

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  FlatList,
+  Dimensions,
+  Image
+} from "react-native";
 import axios from "axios";
 
-class Main extends Component {
+const { height, width } = Dimensions.get("window");
+
+export default class Main extends Component {
   state = {
-    isLoading: false
+    isLoading: true,
+    images: []
   };
 
   loadWallpapers = () => {
@@ -12,9 +23,16 @@ class Main extends Component {
       .get(
         "https://api.unsplash.com/photos/random?count=30&client_id=27e89aa468d4038618154ef61fa81d5dab06a5635c5550166cc636c27fa427c9"
       )
-      .then(function(response) {
-        console.log(response.data);
-      })
+      .then(
+        function(response) {
+          console.log(response.data);
+          this.setState({
+            images: response.data,
+            isLoading: false
+          });
+          // access set state
+        }.bind(this)
+      )
       .catch(function(error) {
         console.log(error);
       })
@@ -27,13 +45,33 @@ class Main extends Component {
     this.loadWallpapers();
   }
 
+  renderItem = (image) => {
+    return (
+      <View style={{ height, width }}>
+        <Image
+          style={styles.renderImage}
+          source={{ uri: image.urls.regular }}
+          resizeMode="cover"
+        />
+      </View>
+    );
+  };
+
   render() {
     return this.state.isLoading ? (
       <View style={styles.root}>
         <ActivityIndicator size="large" color="grey" />
       </View>
     ) : (
-      <View style={styles.loading} />
+      <View style={styles.loading}>
+        <FlatList
+          horizontal
+          pagingEnabled
+          data={this.state.images}
+          renderItem={({ item }) => this.renderItem(item)}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
     );
   }
 }
@@ -48,6 +86,11 @@ const styles = StyleSheet.create({
   loading: {
     flex: 1,
     backgroundColor: "black"
+  },
+  renderImage: {
+    flex: 1,
+    height: null,
+    width: null
   }
 });
 
